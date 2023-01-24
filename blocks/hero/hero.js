6bef15c3140e95b80dd5a237328f6e4c9aa6cc62
Nicolas Barbe <barbe@adobe.com>
2023-01-24 10:33:47 +0100

    fix image ilnk

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -5,21 +5,17 @@ export default function decorate(block) {
   // mj-hero has an issue with outlook in mode='fluid-height' https://github.com/mjmlio/mjml/issues/1253
   // the recommendation is to use a section instead
   // the 2nd column is used to give the section a min-height
-  const backUrl = new window.URL(`${window.hlx.codeBasePath}/icons/hero-background.png`, window.location.href);
+
   const logoUrl = new window.URL(`${window.hlx.codeBasePath}/icons/logo.png`, window.location.href);
 
   return `
     <mj-section mj-class="mj-hero"  background-url="${img.src}"  background-size="contains">
-        <mj-column width="100%">
-            <mj-section mj-class="mj-hero-content" background-url="${backUrl}"  background-size="76%">
-                <mj-column width="100%" align="left">
-                    <mj-image align="left" mj-class="mj-hero-image" src="${logoUrl}" />
-                    <mj-text mj-class="mj-hero-text" width="320px">${h1.innerHTML}</mj-text>
-                </mj-column>
-                <mj-column width="0%">
-                    <mj-spacer mj-class="mj-hero-min-height" />
-                </mj-column>
-            </mj-section>
+        <mj-column width="100%" align="left">
+            <mj-image align="left" mj-class="mj-hero-image" src="${logoUrl}" />
+            <mj-text mj-class="mj-hero-text" width="320px">${h1.innerHTML}</mj-text>
+        </mj-column>
+        <mj-column width="0%">
+            <mj-spacer mj-class="mj-hero-min-height" />
         </mj-column>
     </mj-section>
     `;
